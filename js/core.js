248285de5439eab18696cf4a9b15b85d318b6642
EJ.M <mambo1234554321@gmail.com>
2017-05-06 10:02:58 +0600

    Объединил верстку, добавил папку для тех.работ

--- js/core.js
+++ js/core.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+	// start-header
+	$('#menu-btn').click(function(){		
+			$('.main-nav').slideToggle(400);	
+			if ($(this).hasClass('not-active')) {
+				$(this).addClass('is-active').removeClass('not-active');
+			}else{
+				setTimeout(function(){
+					$('.is-active').addClass('not-active').removeClass('is-active')
+				},600)			
+			}		
+		});
+	// end-header
 
-
+	// start-Info-block
+	$('.slider-1').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: true,
+		asNavFor: '.slider-nav'
+	});
+	$('.slider-nav').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		asNavFor: '.slider-1',
+		dots: true,
+		centerMode: false,
+		focusOnSelect: true
+	});
+	// end-info-block
 });
