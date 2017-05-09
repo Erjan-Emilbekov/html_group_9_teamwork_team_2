45d3c8253a05ccbaa40f294e69fd273bbd082903
Muhitdin <muhitdin0@gmail.com>
2017-05-09 15:48:28 +0600

    сделал валидацию формы

--- js/core.js
+++ js/core.js
@@ -28,5 +28,41 @@ $(document).ready(function(){
 		focusOnSelect: true
 	});
 
+});
+$(document).ready(function() {
+
+	$("#send_form").validate({
+		errorClass: "invalid",
+		validClass: "success",
+
+		rules: {
+			name:{
+				required:true,
+			},
+			lname:{
+				required:true,
+			},
+
+			email:{
+				required:true,
+				email:true
+			}
+		},
+
+			messages: {     
+     email: {
+       required: "Please specify your Email",
+       email: "You entered an invalid Email"
+     },
+     name:{
+     	required:"please write your name",
+     },
+      lname:{
+     	required:"please write your last name",
+     },
+   },
+   errorElement:"em"
+
+	});
 
 });
