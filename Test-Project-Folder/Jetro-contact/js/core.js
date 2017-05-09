ab2cd78e6d4b7936d3c26d739e1135bebd44ecc5
Muhitdin <muhitdin0@gmail.com>
2017-05-09 15:26:28 +0600

    доделал страницы контакт и айтем

--- Test-Project-Folder/Jetro-contact/js/core.js
+++ Test-Project-Folder/Jetro-contact/js/core.js
@@ -6,5 +6,38 @@ $(document).ready(function() {
 		infinite: true,
 
 	});
+	$("#send_form").validate({
+		errorClass: "invalid",
+		validClass: "success",
+
+		rules: {
+			name:{
+				required:true,
+			},
+			lname:{
+				required:true,
+			},
+
+			email:{
+				required:true,
+				email:true
+			}
+		},
+
+			messages: {     
+     email: {
+       required: "Please specify your Email",
+       email: "You entered an invalid Email"
+     },
+     name:{
+     	required:"please write your name",
+     },
+      lname:{
+     	required:"please write your last name",
+     },
+   },
+   errorElement:"em"
+
+	});
 
 });
